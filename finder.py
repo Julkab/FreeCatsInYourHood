fd6f0220f80fcf295980ed99301371e839995459
Julia <juliabalinska99@gmail.com>
2023-05-12 20:29:10 +0200

    added sorting and code refactor

--- finder.py
+++ finder.py
@@ -7,33 +7,45 @@ from selenium.webdriver.common.action_chains import *
 from selenium.webdriver.support.ui import Select 
 import time
 
+class finder():
+    city = None
+    def scraping(self): 
+        options = Options()
+        options.add_experimental_option("detach", True)
+        PATH = "C:\Program Files (x86)\chromedriver.exe"
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                options=options)
 
+        driver.get("https://www.olx.pl")
+        driver.maximize_window()
 
-options = Options()
-options.add_experimental_option("detach", True)
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=options)
+        menu = driver.find_element(By.ID, "headerSearch")
+        location = driver.find_element(By.ID, "cityField")
+        search = driver.find_element(By.ID, "submit-searchmain")
+        links = driver.find_elements(By.ID, "onetrust-accept-btn-handler")
+        
+        for link in links:
+            link.click()
+        actions = ActionChains(driver)
+        actions.click(menu)
+        actions.send_keys("koty do adopcji")
+        actions.perform()
+        actions.click(location)
+        actions.send_keys(self.city)
+        actions.perform()
+        time.sleep(2)
+        actions.click(search)
+        actions.perform()
+        time.sleep(2)
+        sorting = driver.find_element(By.CSS_SELECTOR, "[data-testid=\"sorting-wrapper\"]")
+        actions.click(sorting)
+        actions.perform()
+        sorting_options = driver.find_elements(By.CSS_SELECTOR, "[data-testid=\"sorting-option\"]")
+        for sorting_option in sorting_options:
+            if sorting_option.text == 'Najnowsze':
+                actions.click(sorting_option)
+                actions.perform()
+                break
 
-driver.get("https://www.olx.pl")
-driver.maximize_window()
-
-menu = driver.find_element(By.ID, "headerSearch")
-location = driver.find_element(By.ID, "cityField")
-search = driver.find_element(By.ID, "submit-searchmain")
-links = driver.find_elements(By.ID, "onetrust-accept-btn-handler")
-for link in links:
-    link.click()
-actions = ActionChains(driver)
-actions.click(menu)
-actions.send_keys("koty do adopcji")
-actions.perform()
-actions.click(location)
-actions.send_keys("wrocław")
-actions.perform()
-time.sleep(2)
-actions.click(search)
-actions.perform()
-time.sleep(6)
-
-
+    def __init__(self, city):
+        self.city = city
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import tkinter as tk
 from typing import Self
 # pip3 install customtkinter
 import customtkinter
+from finder import finder
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 class App(customtkinter.CTk):
+    city = None
     def __init__(self):
         super().__init__()
         self.geometry("350x200")
@@ -20,11 +22,13 @@ class App(customtkinter.CTk):
         self.qwery1 = customtkinter.CTkLabel(self, text="Will you be a good cat owner?:")
         self.qwery1.place(relx=0.5, rely=0.5, anchor=customtkinter.S)
 
+
     def yes(self):
         dialog = customtkinter.CTkInputDialog(text="Enter the city you live:", title="Free Cats In Your Hood")
-        print("city:", dialog.get_input())
+        self.city = dialog.get_input()
+        finder_object = finder(self.city)
+        finder_object.scraping()
         
 customtkinter.set_appearance_mode("system")
 app = App()
 app.mainloop()
-
